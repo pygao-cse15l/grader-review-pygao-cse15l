cd686fc493ec13db406b4db275a38276d678d48e
Spacefroyo <petergao322@gmail.com>
2023-02-15 15:57:38 -0800

    Done

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,6 +3,17 @@ import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
 
+class PalindromeChecker implements StringChecker {
+  public boolean checkString(String s) {
+      for (int i = 0; i < s.length()/2; i++) {
+          if (s.charAt(i) != s.charAt(s.length()-1-i)) {
+              return false;
+          }
+      }
+      return true;
+  }
+}
+
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
     return s.equalsIgnoreCase("moon");
@@ -18,4 +29,27 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+    public void testFilter() {
+        List<String> input1 = Arrays.asList("a", "b", "ab", "bcb");
+        StringChecker input2 = new PalindromeChecker();
+
+        assertArrayEquals(new String[]{"a", "b", "bcb"}, ListExamples.filter(input1, input2).toArray());
+    }
+    
+    @Test
+    public void testFilter2() {
+        List<String> input1 = Arrays.asList("a", "ba", "aba", "ca", "a");
+        StringChecker input2 = new PalindromeChecker();
+        assertArrayEquals(new String[]{"a", "aba", "a"}, ListExamples.filter(input1, input2).toArray());
+    }
+
+    @Test
+    public void testMerge() {
+        List<String> input1 = Arrays.asList();
+        List<String> input2 = Arrays.asList("b");
+
+        assertArrayEquals(new String[]{"b"}, ListExamples.merge(input1, input2).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -3,3 +3,31 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [ ! -e student-submission/ListExamples.java ]
+then
+    echo "File ListExamples.java not found"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java ./
+
+javac ListExamples.java
+COMPILE_CODE=$?
+
+if [ $COMPILE_CODE -ne 0 ]
+then
+    echo "ListExamples.java compile failed with exit code $COMPILE_CODE"
+    exit $COMPILE_CODE
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar TestListExamples.java
+COMPILE_CODE=$?
+
+if [ $COMPILE_CODE -ne 0 ]
+then
+    echo "Tester compile failed with exit code $COMPILE_CODE"
+    exit $COMPILE_CODE
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
